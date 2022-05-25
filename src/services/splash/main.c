730ebafaae71c57c825455b52ec0da84deca2ebb
obiwac <obiwac@gmail.com>
2022-05-25 12:35:40 +0200

    splash: Now works with `vt` driver

--- src/services/splash/main.c
+++ src/services/splash/main.c
@@ -1,8 +1,11 @@
 
 #include <err.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/mman.h>
 #include <sys/fbio.h>
@@ -17,89 +20,73 @@
 
 flag on_start;
 
+#define VT_DEV_PREFIX "/dev/ttyv"
+
 int start(void) {
-	// make sure the VESA kernel module is loaded
+	// find an appropriate virtual terminal
+	// usually, you'd want to use VT_OPENQRY to find an available virtual terminal to switch to, but that seems to be acting a little weird (see tests/vt-test)
 
-	if (kldload("vesa") < 0) {
-		if (errno == EEXIST) {
-			warnx("can't load vesa: module already loaded or in kernel");
-		}
+	int default_fd = open(VT_DEV_PREFIX"0", O_RDWR | O_NDELAY, 0);
 
-		else if (errno == ENOEXEC) {
-			warnx("an error occurred while loading vesa kernel module; please check dmesg(8) for details");
-		}
-
-		else {
-			warnx("can't load vesa kernel module");
-		}
-	}
-
-	// find highest resolution appropriate mode (cf. 'usr.sbin/vidcontrol/vidcontrol.c:show_mode_info')
-
-	int mode = -1;
-
-	uint32_t x_res = 0;
-	uint32_t y_res = 0;
-
-	for (size_t i = 0; i <= M_VESA_MODE_MAX; i++) {
-		video_info_t info = { .vi_mode = i };
-
-		if (ioctl(0, CONS_MODEINFO, &info) || \
-			info.vi_mode != i || \
-			!info.vi_width || !info.vi_height || !info.vi_cwidth || !info.vi_cheight // TODO this comparison is incorrect in the aquabsd_vga backend in the aquabsd.alps.vga device
-		) {
-			continue;
-		}
-
-		// found video mode, check if it's the one we want
-
-		if (!(info.vi_flags & V_INFO_GRAPHICS)) {
-			continue;
-		}
-
-		if (info.vi_depth != 32) {
-			continue;
-		}
-
-		if (info.vi_height < y_res) {
-			continue;
-		}
-
-		if (info.vi_height == y_res && info.vi_width < x_res) {
-			continue;
-		}
-
-		mode = i;
-
-		x_res = info.vi_width;
-		y_res = info.vi_height;
-	}
-
-	if (mode < -1) {
-		warnx("can't find appropriate mode");
+	if (default_fd < 0) {
+		warnx("open: %s", strerror(errno));
 		return -1;
 	}
 
-	// switch to that video mode
+	int vt;
 
-	video_info_t info = { .vi_mode = mode };
-	ioctl(0, CONS_MODEINFO, &info);
+	if (ioctl(default_fd, VT_GETACTIVE, &vt) < 0) {
+		warnx("VT_GETACTIVE: %s", strerror(errno));
+		return -1;
+	}
 
-	int prev_mode;
-	ioctl(0, CONS_GET, &prev_mode);
+	close(default_fd);
 
-	ioctl(0, KDENABIO, 0);
-	ioctl(0, VT_WAITACTIVE, 0);
+	// found virtual terminal, activate it
 
-	ioctl(0, KDSETMODE, KD_GRAPHICS);
-	ioctl(0, _IO('V', info.vi_mode - M_VESA_BASE), 0);
+	char* vt_path;
+	asprintf(&vt_path, VT_DEV_PREFIX"%d", vt);
 
-	video_adapter_info_t adapter_info = { 0 };
-	ioctl(0, CONS_ADPINFO, &adapter_info);
+	int fd = open(vt_path, O_RDWR | O_NDELAY, 0);
+	free(vt_path);
 
-	ioctl(0, CONS_SETWINORG, 0);
+	if (fd < 0) {
+		warnx("open: %s", strerror(errno));
+		return -1;
+	}
 
-	uint8_t* fb = mmap(NULL, adapter_info.va_window_size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, 0, 0);
+	if (ioctl(fd, VT_ACTIVATE, vt) < 0) {
+		warnx("VT_ACTIVATE: %s", strerror(errno));
+		return -1;
+	}
+
+	if (ioctl(fd, VT_WAITACTIVE, vt) < 0) {
+		warnx("VT_WAITACTIVE: %s", strerror(errno));
+		return -1;
+	}
+
+	if (ioctl(fd, KDSETMODE, KD_GRAPHICS) < 0) {
+		warnx("KDSETMODE: %s", strerror(errno));
+		return -1;
+	}
+
+	// get window size
+
+	uint32_t x_res = 1280;
+	uint32_t y_res = 1024;
+
+	size_t win_size = x_res * y_res * 4;
+
+	// map framebuffer memory
+
+	void* fb_start;
+
+	if (ioctl(fd, FBIO_GETDISPSTART, &fb_start) < 0) {
+		warnx("FBIO_GETDISPSTART: %s", strerror(errno));
+		return -1;
+	}
+
+	uint8_t* fb = mmap(fb_start, win_size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, 0, 0);
 
 	if (fb == MAP_FAILED) {
 		warnx("framebuffer memory map failed: %s\n", strerror(errno));
@@ -108,7 +95,7 @@ int start(void) {
 
 	// clear screen
 
-	memset(fb, 0, x_res * y_res * 4);
+	memset(fb, 0, win_size);
 
 	// draw logo to screen
 
